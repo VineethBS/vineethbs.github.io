9bd8d504abfb1147b7d8006e7da65fbe492dbdb2
vineeth <vineeth@iist>
2019-06-07 16:16:55 +0530

    AV324, AV343, AV499, AVD871 material removed, student list updated

--- 2019_AV499_AVD871/ProgrammingAssignments/simulate_system.m
+++
@@ -1,9 +0,0 @@
-function reward = simulate_system(action)
-if action == 1
-    reward = 10 * rand(1);
-elseif action == 2
-    reward = exprnd(5);
-elseif action == 3
-    reward = exprnd(2.5);
-end
-        
